bcb30a0645ac314dc5f0f06198a799dba0022ffd
Dmitry <dmitry.votchitsev@gmail.com>
2022-11-24 19:47:08 +0200

    move app to index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,38 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+import Header from './components/Header/Header';
+import Banner from './components/Banner/Banner';
+import Main from './components/Main/Main';
+import TopSales from './components/TopSales/TopSales';
+import Catalog from './components/Catalog/Catalog';
+import Footer from './components/Footer/Footer';
+import About from './components/About/About';
+import Contacts from './components/Contacts/Contacts';
+import NotFoundPage from './components/NotFoundPage/NotFoundPage';
 import './index.css';
 
 const root = createRoot(document.getElementById('root'));
 
 root.render(
-  <App />
+  <BrowserRouter>
+    <Header />
+    <Main>
+      <Banner />
+      <Routes>
+        <Route exact path='/' element={ 
+          <>
+            <TopSales />
+            <Catalog />
+          </> 
+        }/>
+        <Route path='/catalog' element={ <Catalog /> } />
+        <Route path='/about' element={ <About /> } />
+        <Route path='/contacts' element={ <Contacts /> } />
+        <Route path='*' element={ <NotFoundPage />} />
+      </Routes>
+    </Main>
+    <Footer />
+  </BrowserRouter>
 );
