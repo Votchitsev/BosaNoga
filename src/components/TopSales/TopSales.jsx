f55e24fe9b1d4e2c607554cf1b6db0c05bec96ca
Dmitry <dmitry.votchitsev@gmail.com>
2022-11-29 19:17:13 +0200

    Add Top Sales request

--- src/components/TopSales/TopSales.jsx
+++ src/components/TopSales/TopSales.jsx
@@ -1,14 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import request from '../../api/request';
+import Card from '../Catalog/Card';
 
 export default function TopSales() {
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    request('/api/top-sales', 'GET')
+      .then((response) => response.json())
+      .then((json) => setItems(json));
+  }, []);
+
   return (
     <section className="top-sales">
       <h2 className="text-center">Хиты продаж!</h2>
-      <div className="preloader">
+      {/* <div className="preloader">
         <span />
         <span />
         <span />
         <span />
+      </div> */}
+      <div className="row">
+        { items.map((item) => (
+          <Card
+            key={item.id}
+            title={item.title}
+            price={item.price}
+            img={item.images[0]}
+          />
+        ))}
       </div>
     </section>
   );
